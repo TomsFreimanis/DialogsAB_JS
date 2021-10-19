95056c215e2fb52ae460d83b56293ba87c422701
TomsFreimanis <Tomsfreimanis21@gmail.com>
2021-10-19 18:37:05 +0300

    deeper is harder

--- Macibam/Class_10/if_test1.js
+++ Macibam/Class_10/if_test1.js
@@ -9,17 +9,18 @@
  var conclusion = "";
 
  if (a > b) {
-     conclusion = "a ("+ a +") is bigger than  ("+ b +")"; 
+     conclusion = "a ("+ a +") is bigger than ("+ b +")"; 
  }
  else if (a < b) {
-    conclusion = " a ("+ a +") is smaller than  b ("+ b +")";
-
- }
- else {
+    conclusion = " a ("+ a +") is smaller than ("+ b +")";
+}
+ else { 
      conclusion = " a ("+ a + ") is equal to be (" + b + ")";
  }
-     document.getElementById("demo").innerHTML = conclusion;
 
+ {
+     document.getElementById("demo").innerHTML = conclusion;
+ }
   function compare_three() {
  var a =  document.getElementById("aval").value;
  var b =  document.getElementById("bval").value;
@@ -34,7 +35,7 @@ if (b >c) {temp = b; b = c; c = temp;}
 if (a > b) {temp = a; a = b; b = temp;}
     
 //document.getElementById("demo").innerHTML =array.[0] + " " + array[1] + " " + array[2];
-
+{
 document.getElementById("demo").innerHTML = a + " " + " " + b + " " + c ;
 }
 
