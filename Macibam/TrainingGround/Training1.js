9870d02b11d1038bb55a66906c36698a66e4d2e6
TomsFreimanis <Tomsfreimanis21@gmail.com>
2021-10-23 02:29:51 +0300

    slowly

--- Macibam/TrainingGround/Training1.js
+++ Macibam/TrainingGround/Training1.js
@@ -1,14 +1,50 @@
-let text;
-var day = new Date().getDay();
-  if (day == 4|| day ==5) {
-    text = "Looking forward to the Weekend";
-  }
 
-  else if (day == 0 || day == 6) {
-    text = "its weekend";
-  }
-   
-  else {
-    text = "Looking forward to the weekend";
+/*var day = new Date().getDay();
+if (day == 4 || day == 5) {
+    text = "gaidam nedeļas nogali";
 }
-document.getElementById("demo").innerHTML = text;
+   else if  (day == 0 || day == 6 ) {
+        text = "nogale ir pienākusi ";
+    }
+    else {
+        text = "vel tālu lidz brīvdienam"
+    }
+    document.getElementById("demo").innerHTML = text; */
+
+
+    const time = new Date().getHours();
+    let greeting;
+    if (time == 10 || time == 02) {
+      greeting = "Good morning";
+    } else if (time == 20 || time == 21) {
+      greeting = "Good day";
+    } else {
+      greeting = "Good evening";
+    }
+    document.getElementById("demo").innerHTML = greeting; 
+
+   /* let day;
+switch (new Date().getDay()) {
+   
+  case "svēdiena":
+    day = "svēdiena";
+    break;
+  case "Pirmdiena":
+    day = "Pirmdiena";
+    break;
+  case "Otrdiena":
+    day = "Otrdiena";
+    break;
+  case "trešdiena":
+    day = "trešdiena";
+    break;
+  case "cetrudiena":
+    day = "ceturdiena";
+    break;
+  case "piekdiena":
+    day = "piektdiena";
+    break;
+  case  "setsdiena":
+    day = "sestdiena";
+}
+document.getElementById("demo").innerHTML = "šodien ir " + day +  "!!!"; */
--- Macibam/TrainingGround/readmeTraining.md
+++ Macibam/TrainingGround/readmeTraining.md
@@ -1,3 +1,22 @@
 # Pieraksti, kas noderēs
+- One of many JavaScript HTML methods is getElementById(). - The example below "finds" an HTML element (with id="demo"), and changes the element content (innerHTML) to "Hello JavaScript":
+-In HTML, JavaScript code is inserted between <script> and </script> tags. - Scripts can be placed in the <body>, or in the <head> section of an HTML page, or in both.
+- In this example, a JavaScript function is placed in the <body> section of an HTML page.
+- The function is invoked (called) when a button is clicked: [https://www.w3schools.com/js/js_whereto.asp]
+## JavaScript Keywords
+- var	Declares a variable
+    let	Declares a block variable
+    const	Declares a block constant
+    if	Marks a block of statements to be executed on a condition
+    switch	Marks a block of statements to be executed in different cases
+    for	Marks a block of statements to be executed in a loop
+    function	Declares a function
+    return	Exits a function
+    try	Implements error handling to a block of statements
+   ## JavaScript Variables
+   - Using var
+    Using let
+    Using const
+
 - The logical OR (||) operator 
 - The equality operator (==); console.log(1 == 1); console.log('hello' == 'hello');console.log('1' ==  1); console.log(0 == false);
