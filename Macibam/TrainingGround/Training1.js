253c956bda6da6be04223ebed65da3e8419a399b
TomsFreimanis <Tomsfreimanis21@gmail.com>
2021-10-23 20:22:52 +0300

    Novadi test

--- Macibam/TrainingGround/Training1.js
+++ Macibam/TrainingGround/Training1.js
@@ -12,7 +12,7 @@ if (day == 4 || day == 5) {
     document.getElementById("demo").innerHTML = text; */
 
 
-    const time = new Date().getHours();
+   /* const time = new Date().getHours();
     let greeting;
     if (time == 10 || time == 02) {
       greeting = "Good morning";
@@ -21,7 +21,7 @@ if (day == 4 || day == 5) {
     } else {
       greeting = "Good evening";
     }
-    document.getElementById("demo").innerHTML = greeting; 
+    document.getElementById("demo").innerHTML = greeting; */
 
    /* let day;
 switch (new Date().getDay()) {
@@ -48,3 +48,28 @@ switch (new Date().getDay()) {
     day = "sestdiena";
 }
 document.getElementById("demo").innerHTML = "šodien ir " + day +  "!!!"; */
+
+
+
+function myFunction() {
+  var text;
+  var novads = prompt("What's your favorite cocktail drink?", "Latgale");
+  switch(novads) {
+    case "Vidzeme":
+      text = "(lībiešu: Vidūmō[1]) ir viena no latviešu vēsturiskajām zemēm.[2] No Latgales to atdala Pededzes un Aiviekstes upes, no Sēlijas un Zemgales — Daugava, bet ziemeļos tā robežojas ar Igaunijas Republiku";
+      break;
+    case "Latgale":
+      text = "(latgaliešu: Latgola) ir viena no latviešu vēsturiskajām zemēm.[1] Latgales teritoriālā identitāte izsekojama līdz Jersikas ķēniņa valsts laikiem 12. un 13. gadsimta mijā, kas latīņu tekstos saukta par „Letiju” (Lettia)";
+      break;
+    case "Kurzeme":
+      text = "(lībiešu: Kurāmō[1]) jeb Kursa ir viena no latviešu vēsturiskajām zemēm.[2] Mūsdienu Kurzemes teritoriālā identitāte izsekojama kopš Kurzemes bīskapijas dibināšanas 1230. gadā?";
+      break;
+    case "Zemgale":
+      text = "ir viena no latviešu vēsturiskajām zemēm[1], kas agrāk bija cieši saistīta ar Kurzemes un Sēlijas zemēm, tādēļ to robežas ir izplūdušas. Lielākā daļa Zemgales teritorijas ir līdzenums, atšķirībā no pārējām latviešu vēsturiskajām zemēm, kurās ir arī augstienes.";
+      Break;
+
+    default:
+      text = "Tāda novada nav vai tu nepareizi ierakstīji. :(";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
